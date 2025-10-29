dc7f002f691c705cddd9cb2a5888ab5e837c7d92
Ayush <sudhirsoni9889@gmail.com>
2025-10-29 20:01:55 +0530

    learn about models in langchain

---
+++ EmbeddingModals/embedding_huggingface.py
@@ -0,0 +1,23 @@
+# from langchain_huggingface import HuggingFaceEmbeddings
+
+
+# embedding = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
+# text = "Delhi is the captial of India"
+
+# vector = embedding.embed_query(text)
+
+# print(str(vector))
+
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+embeddings = HuggingFaceEndpointEmbeddings(
+    model="sentence-transformers/all-MiniLM-L6-v2",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+
+vector = embeddings.embed_documents(["Delhi is the capital of India","hello"])
+print(str(vector))  # print first 10 values
