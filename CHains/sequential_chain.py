37f3fcd86558e9027f68f841c8ba8646036fb64a
Ayush <sudhirsoni9889@gmail.com>
2025-11-05 00:05:01 +0530

    sequential_chain and parallel_chain

---
+++ CHains/sequential_chain.py
@@ -0,0 +1,27 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Generate a report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template="Generate a 5 line  summary of {text} paragraph",
+    input_variables=['text']
+)
+
+
+parser = StrOutputParser()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+result = chain.invoke({"topic":"cricket"})
+
+print(result)
